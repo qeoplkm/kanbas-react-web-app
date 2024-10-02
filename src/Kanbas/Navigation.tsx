bcbe1274229564ac112fa6a023c291526a525b8c
qeoplkm <jeremyforwork121@gmail.com>
2024-10-01 20:27:39 -0400

    a2

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -11,7 +11,7 @@ export default function KanbasNavigation() {
       <a id="wd-neu-link" target="_blank"
         href="https://www.northeastern.edu/"
         className="list-group-item bg-black border-0 text-center">
-        <img src="/images/NEU.png" width="75px" /></a>
+        <img src="/images/neu.jpg" width="75px"/></a>
       <Link to="/Kanbas/Account" id="wd-account-link"
         className="list-group-item text-center border-0 bg-black text-white">
         <FaRegCircleUser className="fs-1 text text-white" /><br />
