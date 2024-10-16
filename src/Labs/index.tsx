b94c7e36cee779756b5921b399e7fd7924c9c5a4
qeoplkm <jeremyforwork121@gmail.com>
2024-10-15 20:33:11 -0400

    a3

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,7 +7,7 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
-      <h2>Shuai Yao</h2>
+      <h2>Shuai Yao (Jeremy)</h2>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
