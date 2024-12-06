c8be6ec616ae68965eb8c9313a6a909f9b9e688d
qeoplkm <jeremyforwork121@gmail.com>
2024-12-06 00:14:08 -0500

    a6

--- src/Kanbas/Account/Session.tsx
+++ src/Kanbas/Account/Session.tsx
@@ -2,13 +2,15 @@ import * as client from "./client";
 import { useEffect, useState } from "react";
 import { setCurrentUser } from "./reducer";
 import { useDispatch } from "react-redux";
+import axiosWithCredentials from "./client";
+
 export default function Session({ children }: { children: any }) {
     const [pending, setPending] = useState(true);
     const dispatch = useDispatch();
-    const fetchProfile = async () => {
+        const fetchProfile = async () => {
         try {
-            const currentUser = await client.profile();
-            dispatch(setCurrentUser(currentUser));
+            const currentUser = await axiosWithCredentials.post('/api/users/profile');
+            dispatch(setCurrentUser(currentUser.data));
         } catch (err: any) {
             console.error(err);
         }
--- src/Kanbas/Account/client.tsx
+++ src/Kanbas/Account/client.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
+export default axiosWithCredentials;
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
